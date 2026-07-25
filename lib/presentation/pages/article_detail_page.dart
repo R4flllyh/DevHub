e7f9f1cb0e787301d33c3bee80c63f71bd6e5bcd
Raflllyh <Rutherford9908@gmail.com>
2026-07-26 00:46:32 +0700

    feat: Integrate share_plus for article sharing functionality

--- lib/presentation/pages/article_detail_page.dart
+++ lib/presentation/pages/article_detail_page.dart
@@ -10,6 +10,7 @@ import 'package:dev_news/domain/entities/article_entity.dart';
 import 'package:dev_news/presentation/blocs/article_detail/article_detail_bloc.dart';
 import 'package:dev_news/presentation/blocs/article_detail/article_detail_event.dart';
 import 'package:dev_news/presentation/blocs/article_detail/article_detail_state.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:dev_news/presentation/blocs/article_comment/article_comment_bloc.dart';
@@ -59,6 +60,25 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
           onPressed: () => Navigator.pop(context),
         ),
         actions: [
+          //Button Share Article
+          IconButton(
+            icon: const Icon(
+              Icons.share_outlined,
+              color: Color(0xFF1A1A1A),
+              size: 22,
+            ),
+            onPressed: () {
+              final String articleUrl = widget.article.url.isNotEmpty
+                  ? widget.article.url
+                  : 'https://dev.to';
+
+              Share.share(
+                'Check out this article: "${widget.article.title}"\n\n$articleUrl',
+                subject: widget.article.title,
+              );
+            },
+          ),
+          // Button comments
           BlocBuilder<ArticleCommentBloc, ArticleCommentState>(
             builder: (context, commentState) {
               int commentCount = 0;
